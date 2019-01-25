2ba50f33eaf0cd8ffec62e2d66e49d9af9e99b32
nakibkhan <nakib.khan1988@gmail.com>
2019-01-24 19:26:33 -0600

    Nakib (cleanup) removed unused imports

--- src/main/java/com/boot/controller/PhotoAlbumController.java
+++ src/main/java/com/boot/controller/PhotoAlbumController.java
@@ -1,13 +1,9 @@
 package com.boot.controller;
 
-import com.boot.model.PhotoAlbum;
-import com.boot.repository.PhotoAlbumRepository;
 import com.boot.service.PhotoAlbumService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping("/photo-album")
--- src/main/java/com/boot/service/PhotoAlbumService.java
+++ src/main/java/com/boot/service/PhotoAlbumService.java
@@ -5,7 +5,6 @@ import com.boot.repository.PhotoAlbumRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
